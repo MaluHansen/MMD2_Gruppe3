d81e95ce43ada98073fb1c905e39c346ab332eeb
MRBrath93 <147531334+MRBrath93@users.noreply.github.com>
2024-06-05 16:48:04 +0200

    Ændret class

--- assets/js/vin.js
+++ assets/js/vin.js
@@ -31,12 +31,12 @@ function renderDrinksWithTwoPrices(containerToFill, drinks) {
         let prisPrFlaske = drink.acf.pris_pr_flaske ? `${drink.acf.pris_pr_flaske},-` : '-';
         // Der tjekkes om drink.acf.pris_pr_flaske har en "sand" eller "falsk" værdi. Hvis pris_pr_flaske ikke har noget data, erstattes det med "-" og gemmes i en variabel. Hvis den er "sand" dvs. at der er noget data. Så gemmes ${drink.acf.pris_pr_flaske},- i variablen. 
 
-        containerToFill.innerHTML += `<div class="vinEnhed">
+        containerToFill.innerHTML += `<div class="drinkEnhed">
         <div class="titelOgPris">
             <p class="drinkTitle">${drink.acf.navn_pa_drik}</p>
             <p>${prisPrGlas}/${prisPrFlaske}</p>
         </div>
-        <p class="vinBeskrivelse">${drink.acf.detaljerbeskrivelse_om_drikkevaren}</p>
+        <p class="drinkBeskrivelse">${drink.acf.detaljerbeskrivelse_om_drikkevaren}</p>
     </div>
     `
         // Indsætter ovenstående html i den agivet container for hver drink. 
@@ -47,12 +47,12 @@ function renderDrinksWithOnePrice(containerToFill, drinks) {
     drinks.sort((a, b) => a.acf.pris_pr_glas.localeCompare(b.acf.pris_pr_glas));
     // Sortere drinks efter pris_pr_glas - Laveste pris først.
     drinks.forEach(drink => {
-        containerToFill.innerHTML += `<div class="vinEnhed">
+        containerToFill.innerHTML += `<div class="drinkEnhed">
         <div class="titelOgPris">
             <p>${drink.acf.navn_pa_drik}</p>
             <p>${drink.acf.pris_pr_glas},-</p>
         </div>
-        <p class="vinBeskrivelse">${drink.acf.detaljerbeskrivelse_om_drikkevaren}</p>
+        <p class="drinkBeskrivelse">${drink.acf.detaljerbeskrivelse_om_drikkevaren}</p>
     </div>
     `
         // Indsætter ovenstående html i den agivet container for hver drink. 
