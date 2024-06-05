1bf1f4ea80c6db9665ab2c41cc7baef14b31e3e5
MRBrath93 <147531334+MRBrath93@users.noreply.github.com>
2024-06-05 16:28:52 +0200

    Tilføjet kommentarer og kilder

--- assets/js/vin.js
+++ assets/js/vin.js
@@ -22,13 +22,14 @@ function getDrinksByID(Id) {
 }
 
 function renderDrinksWithTwoPrices(containerToFill, drinks) {
-    // Sorter drinks efter pris_pr_flaske som tal
     drinks.sort((a, b) => a.acf.pris_pr_flaske - b.acf.pris_pr_flaske);
+    // Sortere drinks efter pris_pr_flaske - Laveste pris først.
 
     drinks.forEach(drink => {
-        // Hvis pris_pr_glas ikke har noget data, erstat det med "nej". Denne betingelse tjekker om drink.acf.pris_pr_glas har en "sand" værdi.
         let prisPrGlas = drink.acf.pris_pr_glas ? `${drink.acf.pris_pr_glas},-` : '-';
+        // Der tjekkes om drink.acf.pris_pr_glas har en "sand" værdi. Hvis pris_pr_glas ikke har noget data, erstattes det med "-" og gemmes i en variabel. Hvis den er "sand" dvs. at der er noget data. Så gemmes ${drink.acf.pris_pr_glas},- i variablen. 
         let prisPrFlaske = drink.acf.pris_pr_flaske ? `${drink.acf.pris_pr_flaske},-` : '-';
+        // Der tjekkes om drink.acf.pris_pr_flaske har en "sand" eller "falsk" værdi. Hvis pris_pr_flaske ikke har noget data, erstattes det med "-" og gemmes i en variabel. Hvis den er "sand" dvs. at der er noget data. Så gemmes ${drink.acf.pris_pr_flaske},- i variablen. 
 
         containerToFill.innerHTML += `<div class="vinEnhed">
         <div class="titelOgPris">
@@ -38,11 +39,13 @@ function renderDrinksWithTwoPrices(containerToFill, drinks) {
         <p class="vinBeskrivelse">${drink.acf.detaljerbeskrivelse_om_drikkevaren}</p>
     </div>
     `
+        // Indsætter ovenstående html i den agivet container for hver drink. 
     });
 }
 
 function renderDrinksWithOnePrice(containerToFill, drinks) {
     drinks.sort((a, b) => a.acf.pris_pr_glas.localeCompare(b.acf.pris_pr_glas));
+    // Sortere drinks efter pris_pr_glas - Laveste pris først.
     drinks.forEach(drink => {
         containerToFill.innerHTML += `<div class="vinEnhed">
         <div class="titelOgPris">
@@ -52,6 +55,7 @@ function renderDrinksWithOnePrice(containerToFill, drinks) {
         <p class="vinBeskrivelse">${drink.acf.detaljerbeskrivelse_om_drikkevaren}</p>
     </div>
     `
+        // Indsætter ovenstående html i den agivet container for hver drink. 
     });
 }
 
@@ -60,6 +64,7 @@ function renderDrinksWithOnePrice(containerToFill, drinks) {
 getDrinksByID(14)
     .then(drinks => renderDrinksWithTwoPrices(hvidvinEl, drinks))
     .catch(err => console.error("Fejl:", err));
+// Sørger for at funktionerne køre asyncront. Det vil sige at den næste funktion ikke foretages før den foregående er ovre.
 
 getDrinksByID(12)
     .then(drinks => renderDrinksWithTwoPrices(rodvinEl, drinks))
