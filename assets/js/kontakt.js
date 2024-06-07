babb631371121605cca02db3436d0f2090c708af
MaluHansen <90238888+MaluHansen@users.noreply.github.com>
2024-06-07 12:41:43 +0200

    media q

--- assets/js/kontakt.js
+++ assets/js/kontakt.js
@@ -3,9 +3,14 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWFsb3VoYW5zZW4iLCJhIjoiY2x4M2ljczMzMDB4dzJqc
 
 //opretter en var som indeholder kortet igennem Mapbox.
 //alt dato om kortet, fx. style, og positionen bliver vist i den container der har id=kort
-var map = new mapboxgl.Map({
+var kort = new mapboxgl.Map({
   container: "kort",
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [0, 0],
-  zoom: 5
+  center: [9.904599910273452, 57.0536357530417],
+  zoom: 14
 });
+
+//laver en markøre der plaeres på kortet ud fra de kordinater den får
+const lokation = new mapboxgl.Marker({ color: '#933E65'})
+        .setLngLat([9.904599910273452, 57.0536357530417])
+        .addTo(kort);
