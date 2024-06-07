bb8d03c9b9446dece7dd259b7a283327a73941ff
MRBrath93 <147531334+MRBrath93@users.noreply.github.com>
2024-06-07 10:08:38 +0200

    Tilføjet padding

--- assets/js/oel.js
+++ assets/js/oel.js
@@ -1,6 +1,9 @@
 const baseUrl = "https://ullat.marianoergaard.dk/wp-json/wp/v2/posts?per_page=100"
 const fadolFlaskeolVarianterEl = document.querySelector(".fadolFlaskeolVarianter")
 const alkoholfrieOlVarianterEl = document.querySelector(".alkoholfrieOlVarianter")
+const spinnerEl = document.querySelector(".spinner");
+const wineGlassEl = document.querySelector(".wineGlass");
+const burgerEl = document.querySelector(".burger");
 
 let drinkData;
 
