3776c8ae1b59c6a228eda10c76cbd8d971970856
NMaria92 <150144055+NMaria92@users.noreply.github.com>
2024-06-09 12:25:35 +0200

    til top knap ++

--- assets/js/morgen.js
+++ assets/js/morgen.js
@@ -4,36 +4,21 @@ const baseUrl = "https://ullat.marianoergaard.dk/wp-json/wp/v2/posts?per_page=10
 
 const morgenretterEl = document.querySelector(".morgenretter");
 const brunchretterEl = document.querySelector(".brunchretter");
-const spinnerEl = document.querySelector(".spinner");
-const burgerEl = document.querySelector(".burger");
 
 
-
-// spinnerEl.classList.add("show"); // Tilføj klassen "show" til spinnerEl
-// burgerEl.classList.add("show"); // Tilføj klassen "show" til burgerEl
-// wineGlassEl.classList.add("show"); // Tilføj klassen "show" til wineGlassEl
-
-// https://ullat.marianoergaard.dk/wp-json/wp/v2/posts?per_page=100&type-af-maltid=(Indsat Id)
-
-
+// --- MORGENRETTER ---
 // Funktion til at hente data fra Wordpress API'en
-function fetchFood(id) {
-    fetch(baseUrl + `&type-af-maltid=` + id)
-    .then(res => res.json())
-    .then(data => showData(data))
-    .catch(err => console.log("Fejl! Der er desværre sket en fejl.. Vi undskylder mange gange", err));
-    console.log(baseUrl + `&type-af-maltid=` + id)
+function fetchMorgendata() {
+    fetch(baseUrl + `&type-af-maltid=` + 23)
+        .then(res => res.json())
+        .then(data => showMorgenData(data))
+        .catch(err => console.log("Fejl! Der er desværre sket en fejl.. Vi undskylder mange gange", err));
+    console.log(baseUrl + `&type-af-maltid=` + 23)
 }
+fetchMorgendata();
 
-// kalder funktionen fetchFood med id'et 23
-fetchFood(23);
-// kalder funktionen fetchFood med id'et 22
-fetchFood(22);
-
-
-
-function showData(data) {
-    console.log(data);
+// Funktion til at vise data i HTML'en
+function showMorgenData(data) {
     morgenretterEl.innerHTML += `
     `;
 
@@ -47,16 +32,71 @@ function showData(data) {
     // KILDE til ternary operator: Conditional (ternary) operator. Mozilla Corporations. 2024. [online] Accessed 4/6/2024. URL: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator.
     data.forEach(function (morgenData) {
         morgenretterEl.innerHTML += `
-        <div class="enhed">
-            <div class="titelOgPris">
-                <p class="title">${morgenData.acf.navn_pa_ret} ${morgenData.diaet_praeference.length > 0 ? `<i class="material-symbols-outlined">eco</i>` : ``}</p>
-                <p>${morgenData.acf.pris}</p>
-            </div>
+        <div class="titelOgPris">
+        <p class="title">${morgenData.acf.navn_pa_ret} ${morgenData.diaet_praeference.length > 0 ? `<i class="material-symbols-outlined">eco</i>` : ``}</p>
+        <p>${morgenData.acf.pris},- </p>
+        </div>
+        <div class="detaljerOgSupplerende">
         ${morgenData.acf.detaljer_om_retten ? `<p class="beskrivelse">${morgenData.acf.detaljer_om_retten}</p>` : ``}
         </div>
         `;
     })
 }
 
+// --- BRUNCH RETTER ---
+// Funktion til at hente data fra Wordpress API'en
+function fetchBrunchdata() {
+    fetch(baseUrl + `&type-af-maltid=` + 22)
+        .then(res => res.json())
+        .then(data => showBrunchData(data))
+        .catch(err => console.log("Fejl! Der er desværre sket en fejl.. Vi undskylder mange gange", err));
+}
+fetchBrunchdata();
 
-// DOM manipulation 
+// Funktion til at vise data i HTML'en
+function showBrunchData(data) {
+    brunchretterEl.innerHTML += `
+    `;
+
+    data.forEach(function (brunchData) {
+        brunchretterEl.innerHTML += `
+        <div class="pris">
+        <p>${brunchData.acf.pris},-</p>
+        </div>
+        <div class="VariationerAfRetten">
+            ${brunchData.acf.variationer_af_retten.variation_1.beskrivelse_af_retten ? `<p class="beskrivelse">${brunchData.acf.variationer_af_retten.variation_1.beskrivelse_af_retten}</p>` : ``}
+            ${brunchData.acf.variationer_af_retten.variation_2.beskrivelse_af_retten ? `<p class="beskrivelse">${brunchData.acf.variationer_af_retten.variation_2.beskrivelse_af_retten}</p>` : ``}
+            ${brunchData.acf.variationer_af_retten.variation_3.beskrivelse_af_retten ? `<p class="beskrivelse">${brunchData.acf.variationer_af_retten.variation_3.beskrivelse_af_retten}</p>` : ``}
+            ${brunchData.acf.variationer_af_retten.variation_4.beskrivelse_af_retten ? `<p class="beskrivelse">${brunchData.acf.variationer_af_retten.variation_4.beskrivelse_af_retten}</p>` : ``}
+            ${brunchData.acf.variationer_af_retten.variation_5.beskrivelse_af_retten ? `<p class="beskrivelse">${brunchData.acf.variationer_af_retten.variation_5.beskrivelse_af_retten}</p>` : ``}
+            ${brunchData.acf.variationer_af_retten.variation_6.beskrivelse_af_retten ? `<p class="beskrivelse">${brunchData.acf.variationer_af_retten.variation_6.beskrivelse_af_retten}</p>` : ``}
+            ${brunchData.acf.variationer_af_retten.variation_7.beskrivelse_af_retten ? `<p class="beskrivelse">${brunchData.acf.variationer_af_retten.variation_7.beskrivelse_af_retten}</p>` : ``}
+            ${brunchData.acf.variationer_af_retten.variation_8.beskrivelse_af_retten ? `<p class="beskrivelse">${brunchData.acf.variationer_af_retten.variation_8.beskrivelse_af_retten}</p>` : ``}   
+        </div>
+    `;
+    })
+}
+
+
+// --- Til top knap --- 
+// først erklærer vi en variabel ud fra vores HTML dokument som har klassen ".topBtn".
+// Herefter tilføjer vi en eventlistener, som lytter efter scroll.
+// Vi laver et if/else statement, som siger at hvis scrollY er større end 500, så skal knappen vises.
+// Vi tilføjer også en eventlistener, som lytter efter et klik på knappen.
+// Når der klikkes på knappen, skal der scrolles til toppen af siden.
+let toTopBtn = document.querySelector(".topBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 500) {
+        toTopBtn.classList.add("active");
+    } else {
+        toTopBtn.classList.remove("active");
+    }
+
+    toTopBtn.addEventListener("click", () => {
+        document.body.scrollTop = 0; // Safari
+        document.documentElement.scrollTop = 0; // til Chrome, Firefix, IE og Opera
+    });
+});
+// Til top-funktion er udarbejdet med inspiration fra følgende kilde: 
+// How TO - Scroll Back To Top Button. W3School. 2024. [online] Accessed 9/6/2024. URL: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
