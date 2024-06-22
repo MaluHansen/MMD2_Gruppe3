581317badd3517b73187851d84d7a17f27a8a2dc
NMaria92 <150144055+NMaria92@users.noreply.github.com>
2024-06-22 20:31:25 +0200

    rettelser js

---
+++ assets/js/script.js
@@ -0,0 +1,293 @@
+// Array med oversættelser index 0 = DA, index 1 = EN
+const translations = [{
+    "sprog": "DA",
+    "forside": "Forside",
+    "menu": "Menu",
+    "morgen": "Morgen",
+    "middag": "Middag",
+    "aften": "Aften",
+    "sødeSager": "Søde Sager",
+    "cocktails": "Cocktails",
+    "drikke": "Kolde & Varme drikke",
+    "øl": "Øl",
+    "vin": "Vin",
+    "events": "Events",
+    "selskaber": "Selskaber",
+    "madUdAfHuset": "Mad ud af huset",
+    "bookBord": "Book bord",
+    "gavekort": "Gavekort",
+    "kontakt": "Kontakt",
+
+    "gulDrink": "Gul drink med sugerør og blomster placeret med en farverig baggrund",
+    "bookBord": "Book Bord",
+
+    "velkommen": "Velkommen til Café Ulla Terkelsen London",
+    "undertitel": "Få en oplevelse og mærk den gode stemning i behagelige omgivelser",
+    "personale": "Vores personale byder jer varmt velkommen med personlig betjening og med mulighed for at blive serviceret i vores fine lokaler eller i vores skønne gårdhave.",
+    "takeaway": "Er du på farten, eller vil du nyde vores retter i hjemmets trygge rammer, så har du også mulighed for at bestille din mad og drikke som take-away.",
+    "billederammeforside": "Caféen Ulla Terkelsen Londons lokaler set indefra",
+
+    // Cards på forsiden 
+    "menuimg": "En tallerken med forskellige tapasretter",
+    "titlecard1": "Farverig Mad",
+    "contentcard1": "Vi tilbyder et spændende menukort der med brug af lokale råvarer varierer efter sæson og humør. Uanset om du besøger os i caféen eller får maden leveret direkte til døren, garanterer vi smagsoplevelser, du sent vil glemme. Prøv f.eks. vores smagsfulde aftenmenu.",
+
+    "drikkevarerimg": "En kop kaffe inde fra Ulla Terkelsen",
+    "titlecard2": "Lækre Drikkevarer",
+    "contentcard2": "Vi tilbyder drikkevarer for en hver smag. Hvad enten du er til kolde øl, sodavand eller kaffe og the. Vi har byens største udvalg af rød sodavand, og har en bred variation af øl og vin.",
+
+    "eventimg": "Stemningsbillede fra vores event omkring LP-plader",
+    "titlecard3": "Spændende Events",
+    "contentcard3": "Tag dine venner med ned og få en fælles oplevelse. Hos os kan I opleve live-musik og  spændende events hele året rundt. Vores events dækker alt fra musik til kreative initiativer.",
+
+    "cocktailimg": "en cocktail med frugt og pynt",
+    "titlecard4": "Festlige Aftener",
+    "contentcard4": "Fra kl. 21:00 forvandler vores hyggelige café sig fra spisested til et magisk og farverigt fest-univers med god musik. Du kan samtidig nyde vores brede udvalg af eksotiske cocktails og klassiske drinks. Vi har noget for en hver smag.",
+
+    //grøn sektion
+    "ulla": "Ulla elsker Ulla",
+    "grøncontent": "Caféens indretning hylder den danske journalist Ulla Terkelsens lange karriere i London. Vores unikke lokaler har en charmerende blanding af vintage og moderne indretning med mange farver, samt en hyggelig og legende atmosfærer.",
+    "ullaimg": "Et billede af tv-journalisten Ulla Terkelsen fra sine yngre dage",
+
+    // footer 
+    "footertitle1": "Kontakt",
+    "footertitle2": "Åbningstider",
+
+    "mandag": "Mandag",
+    "tirsdag": "Tirsdag",
+    "onsdag": "Onsdag",
+    "torsdag": "Torsdag",
+    "fredag": "Fredag",
+    "lørdag": "Lørdag",
+    "søndag": "Søndag",
+
+    "footertitle3": "Fødevarestyrelsens<br>smiley-ordning",
+    "kontrolimg": "Fødevarestyrelsens kontrolrapport logo",
+
+    "footertitle4": "Følg os her!",
+},
+{
+    "sprog": "EN",
+    "forside": "Frontpage",
+    "menu": "Menu",
+    "morgen": "Morning",
+    "middag": "Noon",
+    "aften": "Evening",
+    "sødeSager": "Sweet Treats",
+    "cocktails": "Cocktails",
+    "drikke": "Cold & Hot Drinks",
+    "øl": "Beer",
+    "vin": "Wine",
+    "events": "Events",
+    "selskaber": "Parties",
+    "madUdAfHuset": "Takeaway",
+    "bookBord": "Book a Table",
+    "gavekort": "Gift Card",
+    "kontakt": "Contact",
+
+    "gulDrink": "Yellow drink with straw and flowers placed with a colorful background",
+    "bookBord": "Book Table",
+
+    "velkommen": "Welcome to Café Ulla Terkelsen London",
+    "undertitel": "Get an experience and feel the good atmosphere in comfortable surroundings", 
+    "personale": "Experience a world of tastes and colors when you step into our café. Our staff warmly welcomes you with personalized service and the option to be served in our fine premises or in our lovely courtyard.",
+    "takeaway": "If you are on the go, or want to enjoy our dishes in the comfort of your home, you also have the option to order your food and drinks as take-away.",
+    "billederammeforside": "The interiors of Café Ulla Terkelsen London",
+
+    // Cards på forsiden
+    "menuimg": "A plate with various tapas dishes",
+    "titlecard1": "Colorful Food",
+    "contentcard1": "We offer an exciting menu that, with the use of local ingredients, varies according to season and mood. Whether you visit us in the café or have the food delivered directly to your door, we guarantee taste experiences you won't forget. Try our flavorful evening menu.",
+
+    "drikkevarerimg": "A cup of coffee inside Ulla Terkelsen",
+    "titlecard2": "Delicious Beverages",
+    "contentcard2": "We offer beverages for every taste. Whether you like cold beer, soda, coffee, or tea. We have the city's largest selection of red soda, and have a wide variety of beer and wine.",
+
+    "eventimg": "Atmosphere from our event about LP records",
+    "titlecard3": "Exciting Events",
+    "contentcard3": "At Café Ulla Terkelsen London, we hold regular events such as wine tastings, concerts, and quiz nights. Keep an eye on our calendar, so you don't miss out on the next event. It's a great way to spend an evening with friends.",
+
+    "cocktailimg": "A cocktail with fruit and decoration",
+    "titlecard4": "Festive Evenings",
+    "contentcard4": "From 9:00 PM, our cozy café transforms from a dining place into a magical and colorful party universe with great music. At the same time, you can enjoy our wide selection of exotic cocktails and classic drinks. We have something for every taste.",
+
+    //grøn sektion
+    "ulla": "Ulla Loves Ulla",
+    "grøncontent": "The café's decor pays tribute to the long career of Danish journalist Ulla Terkelsen in London. Our unique premises feature a charming mix of vintage and modern decor with many colors, as well as a cozy and playful atmosphere.",
+    "ullaimg": "Image of the tv-journalist Ulla Terkelsen",
+
+    // footer 
+    "footertitle1": "Contact",
+    "footertitle2": "Opening Hours",
+
+    "mandag": "Monday",
+    "tirsdag": "Thuesday",
+    "onsdag": "Wednesday",
+    "torsdag": "Thursday",
+    "fredag": "Friday",
+    "lørdag": "Saturday",
+    "søndag": "Sunday",
+
+    "footertitle3": "Food Authority's<br>smiley scheme",
+    "kontrolimg": "Food Authority's control report logo",
+
+    "footertitle4": "Follow us here!",
+}];
+
+
+function insertTranslations(translation) {
+    document.querySelector('nav').innerHTML = `
+        <div class="topNavigation">
+            <a id="logoUlla" href="./index.html"><img class="logo" src="./assets/img/logoUllaT.svg" alt="Ulla Terkelsen London Logo"></a>
+            <i class="fa-solid fa-bars menuIkon" id="burgerMenu"></i>
+        </div>
+        <ul class="navMobil">
+            <li class="activeNavA"><a href="./index.html">Forside</a></li>
+            <li>  
+                <div class="subNav">
+                    <a href="./menu.html">Menu</a>
+                    <ul class="subNavContent">
+                        <li><a href="./menuMorgen.html">${translation.morgen}</a></li>
+                        <li><a href="./menuMiddag.html">${translation.middag}</a></li>
+                        <li><a href="./menuAften.html">${translation.aften}</a></li>
+                        <li><a href="./menuSoedeSager.html">${translation.sødeSager}</a></li>
+                        <li><a href="./menuCocktails.html">${translation.cocktails}</a></li>
+                        <li><a href="./menuDrikke.html">${translation.drikke}</a></li>
+                        <li><a href="./menuOel.html">${translation.øl}</a></li>
+                        <li><a href="./menuVin.html">${translation.vin}</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href="./events.html">${translation.events}</a></li>
+            <li><a href="./selskaber.html">${translation.selskaber}</a></li>
+            <li><a href="./madUdAfHuset.html">${translation.madUdAfHuset}</a></li>
+            <li><a href="./bookBord.html">${translation.bookBord}</a></li>
+            <li><a href="./gavekort.html">${translation.gavekort}</a></li>
+            <li><a href="./kontakt.html">${translation.kontakt}</a></li>
+            <div class="containerLanguage">
+                <li><a class="languageSelector"><i class="material-symbols-outlined">language</i><p class="language">${translation.sprog}</p><i class="material-symbols-outlined">keyboard_arrow_down</i></a></li>
+                <div class="languageDropdown">
+                        <li><a href="#" id="lang-da">Dansk</a></li>
+                        <li><a href="#" id="lang-en">English</a></li>
+                </div>
+                </div>
+        </ul>
+        `;
+
+    document.querySelector('.centerBtn').innerHTML = `
+        <a href="./bookBord.html" class="btn centerBtn">${translation.bookBord}</a>
+        `;
+
+    document.querySelector('.introTekstForside').innerHTML = `
+        <div class="forordForside">
+            <h3>${translation.velkommen}</h3>
+            <p>${translation.undertitel}</p>
+        </div>
+        <p>${translation.personale}</p>
+        <p>${translation.takeaway}</p>
+        `;
+
+    document.querySelector('.billedeRammeForside').innerHTML = `
+            <picture>
+                <source media="(min-width:600px)" srcset="./assets/img/forside/cafeIndefra.jpg">
+                <img src="./assets/img/smaa_Billeder/Forside/cafeIndefra_lille.jpg" alt="${translation.billederammeforside}" loading="lazy">       
+            </picture>    
+        `;
+        
+    document.querySelector('.menukort').innerHTML = `
+        <img src="./assets/img/til_Menukort/tapas.jpeg" alt="${translation.menuimg}" loading="lazy">
+        <p>${translation.titlecard1}</p>
+        <p>${translation.contentcard1}</p>
+        `;
+
+    document.querySelector('.drikkevarer').innerHTML = `
+        <img src="./assets/img/Forside/kaffeIndex.jpg" alt="${translation.drikkevarerimg}" loading="lazy">
+        <p>${translation.titlecard2}</p>
+        <p>${translation.contentcard2}</p>
+        `;
+
+    document.querySelector('.events').innerHTML = `
+        <img src="./assets/img/Forside/events.jpg" alt="${translation.eventimg}" loading="lazy">
+        <p>${translation.titlecard3}</p>
+        <p>${translation.contentcard3}</p>
+        `;
+
+    document.querySelector('.drinks').innerHTML = `
+        <img src="./assets/img/Forside/cocktailMedFrugt.jpg" alt="${translation.cocktailimg}">
+        <p>${translation.titlecard4}</p>
+        <p>${translation.contentcard4}</p>
+        `;
+
+    document.querySelector('.groenSektionUlla').innerHTML = `
+        <div class="groenSektionTekst">
+            <h2>${translation.ulla}</h2>
+            <p>${translation.grøncontent}</p>
+        </div>
+        <img class="UllaTImg" src="./assets/img/Forside/UllaTerkelsenUdenBaggrund.png" alt="${translation.ullaimg}">
+    `;
+
+    document.querySelector('.footerTitle').innerHTML = `
+        <p class="footerTitle">${translation.footertitle1}</p>
+    `;
+
+    document.querySelector('.aabningstider').innerHTML = `
+            <p class="footerTitle">${translation.footertitle2}</p>
+            <div class="aabningstiderContainer">
+                <div id="dage">
+                    <p>${translation.mandag}</p>
+                    <p>${translation.tirsdag}</p>
+                    <p>${translation.onsdag}</p>
+                    <p>${translation.torsdag}</p>
+                    <p>${translation.fredag}</p>
+                    <p>${translation.lørdag}</p>
+                    <p>${translation.søndag}</p>
+                </div>
+                <div id="tidsrum">
+                    <p>Lukket</p>
+                    <p>11.00 - 23.00</p>
+                    <p>11.00 - 23.00</p>
+                    <p>11.00 - 23.00</p>
+                    <p>11.00 - 02.00</p>
+                    <p>11.00 - 02.00</p>
+                    <p>Lukket</p>
+                </div>
+            </div>
+        `;
+        
+    document.querySelector('.andetInfo').innerHTML = `
+            <div id="smiley">
+                <p class="footerTitle">${translation.footertitle3}</p>
+                <a href="https://www.findsmiley.dk/500717" target="_blank"><img src="./assets/img/smileyRapport.png" alt="${translation.kontrolimg}"></a>
+            </div>
+            <div id="socialeMedier">
+                <p class="footerTitle">${translation.footertitle4}</p>
+                <a href="https://www.facebook.com/CafeUlla" aria-label="Et logo til facebook" target="_blank"><i class="fa-brands fa-square-facebook"></i></a>
+                <a href="https://www.instagram.com/cafeullaterkelsenlondon/" aria-label="Et logo til instagram" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+            </div>
+    `;
+
+    let languageOptionDA = document.querySelector('#lang-da');
+    let languageOptionEN = document.querySelector('#lang-en');
+    let languageSelector = document.querySelector('.languageSelector');
+    let languageDropdown = document.querySelector('.languageDropdown');
+    
+    languageOptionDA.addEventListener('click', function () {
+        insertTranslations(translations[0]);
+    });
+    
+    languageOptionEN.addEventListener('click', function () {
+        insertTranslations(translations[1]);
+    });
+    
+    // Function der viser dropdown menuen når der hoveres over classen og skjuler den når der ikke længere hoveres over classen
+    languageSelector.addEventListener('click', function () {
+        if (languageDropdown.style.display == 'block') {
+            languageDropdown.style.display = 'none';
+        } else {
+            languageDropdown.style.display = 'block';
+        }
+    });
+}
+
+// default language DA
+insertTranslations(translations[0]);
